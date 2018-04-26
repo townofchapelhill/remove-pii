ff58139a681a53ecce72f269c1cc21941b5ffedf
Duvanchi <cochize0@gmail.com>
2018-04-26 16:23:23 -0400

    Improved scrub speed, still working on including automation

--- no-pii.py
+++ no-pii.py
@@ -11,24 +11,28 @@ fileName = input("What's the name of the desired file? ")
 df = pandas.read_csv(fileName + ".csv")
 df.head(n=5)
 print(df)
+print("===============================")
 
 # checks with user to confirm if more rows need cleaning
 def checkFinished():
     yesNo = input("Column cleaned. Would you like to scrub another column? ")
-    if yesNo == 'yes':
+    print("============================================================")
+    if yesNo == 'yes' or yesNo == 'Yes':
         scrubData()
     else:
         # writes new .csv file  
         df.to_csv(fileName + "_scrubbed.csv", encoding='utf-8', index = False)
         print('Scrubbed file complete.')
+        print("===============================")
 
 # scrubs the data from the chosen column
 # only does one at a time, working on improving that
 def scrubData():
     columnName = input("Which columns would you like to delete? ")
+    print("===============================")
     print("Scrubbing data... (larger datasets take time)")
-    scrub = lambda x: scrubadub.clean(' ')
-    df[str(columnName)] = df[str(columnName)].apply(scrub)
+    print("===============================")
+    df.drop([columnName], axis=1, inplace = True)
     checkFinished()
 
 # calls scrubData function
